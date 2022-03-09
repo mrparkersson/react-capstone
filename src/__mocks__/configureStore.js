979e7f353f90a6af5faa1929c500a37f9cc9614d
mrparkersson <amoparkerprince78@gmail.com>
2022-03-09 07:39:06 +0000

    Add components

--- src/__mocks__/configureStore.js
+++ src/__mocks__/configureStore.js
@@ -1,7 +1,7 @@
 import { createStore, combineReducers, applyMiddleware } from 'redux';
 import logger from 'redux-logger';
 import thunk from 'redux-thunk';
-import countriesReducer from './countries/countries';
+import countriesReducer from './countries';
 
 const reducer = combineReducers({
   countriesReducer,
