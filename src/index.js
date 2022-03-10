a35991044d6bb2b2d9dc82b74470a7dc0409ce34
mrparkersson <amoparkerprince78@gmail.com>
2022-03-10 08:37:39 +0000

    Fix linter error

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 import React from 'react';
+import App from './App';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import store from './redux/configureStore';
 import './index.css';
-import App from './App';
 
 ReactDOM.render(
   <Provider store={store}>
